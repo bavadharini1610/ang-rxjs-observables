d43b2ab54f42c5a3fe82616f2277ee5c822b9d6a
test <test@tests-MacBook-Pro.local>
2025-08-31 12:49:11 +0530

    creating and using observable with array and as stream of data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,24 @@ export class AppComponent{
   title = 'angular-observables';
 
   data: any[] = [];
+
+
+  //1. create an observable
+
+  //Observable
+  myObservable = new Observable((observer)=>{
+    setTimeout(()=>{observer.next(1)},1000);
+    setTimeout(()=>{observer.next(2)},2000);
+    setTimeout(()=>{observer.next(3)},3000);
+    setTimeout(()=>{observer.next(4)},4000);
+    setTimeout(()=>{observer.next(5)},5000);
+  });
+
+  //subscriber - whenever the observable emits, the subscriber would get notified
+  getAsyncData(){
+    this.myObservable.subscribe((val:any)=>{
+      this.data.push(val);
+    });
+  }
+
 }
