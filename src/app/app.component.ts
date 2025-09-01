a2b1fa88d422203402bf7f2696f0325a0e984364
test <test@tests-MacBook-Pro.local>
2025-09-01 09:02:28 +0530

    fromEvent Operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
-import { from, Observable, of } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { from, fromEvent, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements AfterViewInit{
   title = 'angular-observables';
 
   data: any[] = [];
   array1 = [1,3,5,7,9];
   array2 = ['A','B','C','D','E'];
+
+
+  @ViewChild('createBtn')
+  createBtn:ElementRef;
+  createBtnObs;
+
+
 
 
   //1. create an observable
@@ -64,5 +71,29 @@ export class AppComponent{
       }
     });
   }
+
+  buttonClicked(){
+    let count = 0;
+    this.createBtnObs = fromEvent(this.createBtn.nativeElement,'click')
+                      .subscribe((data)=>{
+                        console.log(data);
+                        this.showItem(++count);
+                      });
+
+  }
+
+  ngAfterViewInit(){
+    this.buttonClicked();
+  }
+
+  showItem(val){
+    let div = document.createElement('div');
+    div.innerHTML = 'Item '+ val;
+    div.className = 'data-list'
+    document.getElementById('container').appendChild(div);
+
+  }
+
+
 
 }
